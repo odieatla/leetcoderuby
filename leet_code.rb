e736918aa577202fd7d55f733019bc3a849de31c
odieatla <ozhang@undertone.com>
2013-03-15 15:35:23 -0700

    Remove Duplicates from Sorted Array

--- leet_code.rb
+++ leet_code.rb
@@ -1,23 +1,70 @@
 class LeetCode
-  def remove_element(A, elem)
-    before = 0
-    after = 0
+  def self.remove_element(a, elem)
+=begin
+*****************************************************************************************************
+  Given an array and a value, remove all instances of that value in place and return the new length.
 
-    return 0 if A.empty?
+  The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+******************************************************************************************************
+=end
 
-    while after < A.size
-      if A[after] != elem
-        A[before] = A[after]
+    return 0 if a.empty?
+
+    index = 0
+=begin
+    while after < a.size
+      if a[after] != elem
+        a[before] = a[after]
         before += 1
       end
 
       after += 1
     end
 
-    A = A[0..(before - 1)]  if before > 0
+    a = a[0..(before - 1)]  if before > 0
+=end
 
-    return before
+    a.each do |v|
+      if v != elem
+        a[index] = v
+        index += 1
+      end
+    end
+
+    a = a[0..(index - 1)] if index > 0
+
+    return a.size
   end # end of method
 
+  def self.remove_duplicates(a)
+=begin
+****************************************************************************************************
+  Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+  Do not allocate extra space for another array, you must do this in place with constant memory.
+
+  For example,
+  Given input array A = [1,1,2],
+
+  Your function should return length = 2, and A is now [1,2].
+****************************************************************************************************
+=end
+    return 0 if a.empty?
+
+    elem = nil
+    index = 0
+
+    a.each do |v|
+      if(v != elem)
+        a[index] = v
+        elem = v
+        index += 1
+      end
+    end
+
+    a = a[0..(index - 1)] if index > 0
+
+    return a.size
+  end
 
 end
